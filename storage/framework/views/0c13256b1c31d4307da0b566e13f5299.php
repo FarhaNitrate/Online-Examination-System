a8332c9cd64c52c72ad8aa75ca8505fa84bbe415
Akibilies20001 <117575565+Akibilies20001@users.noreply.github.com>
2024-03-03 20:49:45 +0600

    Teacher and student profile with login and edit profile

---
+++ storage/framework/views/0c13256b1c31d4307da0b566e13f5299.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo e(url('css/style.css')); ?>">
+
+</head>
+<body>
+    <div class="container p-5">
+        <div class="profile card p-5 rounded text-center">
+
+            <div class="text-center">
+                <img class="logo" src="https://www.bracu.ac.bd/sites/default/files/resources/media/bracu_logo.png">
+            </div>
+    
+            <h3 class="text-center my-5 text-uppercase">Welcome to Student Management System</h3>
+
+            <a class="mt-4" href="<?php echo e(route('login')); ?>"><button class="btn btn-info btn-sm d-inline">Login</button></a>
+        </div>
+    </div>
+</body>
+</html>
+<?php /**PATH D:\Xampp\htdocs\Websites\student-management-system\resources\views/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/aa5ed6dafac044d41b8d8f0cb7df0de1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo e(url('css/style.css')); ?>">
+
+</head>
+<body>
+    <div class="container p-5">
+        <div class="profile card p-5 rounded">
+            <form class="text-right" method="POST" action="<?php echo e(route('logout')); ?>">
+                <?php echo csrf_field(); ?>
+                <button class="btn btn-danger btn-sm" type="submit">Logout</button>
+            </form>
+        
+            <h3 class="text-center">Student Profile</h3>
+
+            <p>Name: <?php echo e($student->name); ?></p>
+            <p>Student Id: <?php echo e($student->uniId); ?></p>
+            <p>Email: <?php echo e($student->email); ?></p>
+            <p>Phone No: <?php echo e($student->phoneNo); ?></p>
+            <p>Department: <?php echo e($student->dept); ?></p>
+
+            <div>
+                <a class="d-inline" href="<?php echo e(route('student.edit')); ?>"><button class="btn btn-info btn-sm d-inline">Edit</button></a>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
+<?php /**PATH D:\Xampp\htdocs\Websites\student-management-system\resources\views/student/index.blade.php ENDPATH**/ ?>
